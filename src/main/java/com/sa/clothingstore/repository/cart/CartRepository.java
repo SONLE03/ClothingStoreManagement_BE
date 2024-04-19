337aa82f5fb196b9edc2c1ddb884f0ec228856ba
SONLE03 <sonle102003@gmail.com>
2024-04-20 00:06:30 +0700

    implement cart service

--- src/main/java/com/sa/clothingstore/repository/cart/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sa.clothingstore.repository.cart;
-
-import com.sa.clothingstore.model.cart.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CartRepository extends JpaRepository<Cart, UUID> {
-}
