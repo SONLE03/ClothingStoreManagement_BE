6b8232f9e1daa1e77856e8befbb8d3ccef9c497c
SONLE03 <sonle102003@gmail.com>
2024-04-19 11:27:06 +0700

    update import service

--- src/main/java/com/sa/clothingstore/controller/importProduct/ImportProductController.java
+++ src/main/java/com/sa/clothingstore/controller/importProduct/ImportProductController.java
@@ -2,6 +2,8 @@ package com.sa.clothingstore.controller.importProduct;
 
 import com.sa.clothingstore.constant.APIConstant;
 import com.sa.clothingstore.dto.request.importProduct.ImportRequest;
+import com.sa.clothingstore.dto.response.importProduct.ImportResponse;
+import com.sa.clothingstore.model.importInvoice.ImportInvoice;
 import com.sa.clothingstore.service.importProduct.ImportProductService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -9,13 +11,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RequestMapping(APIConstant.IMPORTS)
 @RestController
 @AllArgsConstructor
 public class ImportProductController {
-    ImportProductService importService;
+    private final ImportProductService importService;
 
+    @GetMapping()
+    public List<ImportInvoice> getAllImport(){
+        return importService.getAllImport();
+    }
+
+    @GetMapping(APIConstant.IMPORT_ID)
+    public ImportResponse getImportById(@PathVariable UUID importId){
+        return importService.getImportById(importId);
+    }
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public String createImport(@RequestBody List<@Valid ImportRequest> request){
--- src/main/java/com/sa/clothingstore/dto/response/importProduct/ImportResponse.java
+++ src/main/java/com/sa/clothingstore/dto/response/importProduct/ImportResponse.java
@@ -1,8 +1,10 @@
 package com.sa.clothingstore.dto.response.importProduct;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
 public class ImportResponse {
+    @JsonProperty("importId")
     private UUID importId;
-
 }
