996c685d0dec0bb4aedda13dc0bfe9a51e66a234
SONLE03 <sonle102003@gmail.com>
2024-05-24 10:56:36 +0700

    fix bug forgot_password

--- src/main/java/com/sa/clothingstore/model/user/ForgotPassword.java
+++ src/main/java/com/sa/clothingstore/model/user/ForgotPassword.java
@@ -19,7 +19,7 @@ public class ForgotPassword {
 
     private Integer otp;
 
-    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(nullable = false, name = "user_id")
     private User user;
 
